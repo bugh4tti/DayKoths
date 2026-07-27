5b4361f730cd40ef943b2d6a92675d702f02e833
bugh4tti <quieroamiabuu@gmail.com>
2026-07-27 16:40:17 -0300

    Enhance scoreboard with PlaceholderAPI support

--- src/main/java/com/bughatti/daykoths/manager/ScoreboardManager.java
+++ src/main/java/com/bughatti/daykoths/manager/ScoreboardManager.java
@@ -18,9 +18,16 @@ import java.util.stream.Collectors;
 public class ScoreboardManager {
 
     private final DayKoths plugin;
+    private final boolean papiEnabled;
 
     public ScoreboardManager(DayKoths plugin) {
         this.plugin = plugin;
+        this.papiEnabled = plugin.getServer().getPluginManager().isPluginEnabled("PlaceholderAPI");
+    }
+
+    private String applyPapi(Player player, String text) {
+        if (!papiEnabled) return text;
+        return me.clip.placeholderapi.PlaceholderAPI.setPlaceholders(player, text);
     }
 
     public void update(Koth koth, String timeLeft) {
@@ -29,7 +36,6 @@ public class ScoreboardManager {
             return;
         }
 
-        String title = HexUtil.colorize(plugin.getConfig().getString("scoreboard.title", "&b&lDAYKOTHS"));
         List<String> rawLines = plugin.getConfig().getStringList("scoreboard.lines");
 
         List<Map.Entry<UUID, Integer>> top = koth.getScoreProgress().entrySet().stream()
@@ -37,8 +43,12 @@ public class ScoreboardManager {
                 .collect(Collectors.toList());
 
         for (Player player : Bukkit.getOnlinePlayers()) {
+            String title = applyPapi(player, plugin.getConfig().getString("scoreboard.title", "&b&lDAYKOTHS"))
+                    .replace("%koth%", koth.getName())
+                    .replace("%time_left%", timeLeft);
+
             Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
-            Objective obj = board.registerNewObjective("daykoths", "dummy", title);
+            Objective obj = board.registerNewObjective("daykoths", "dummy", HexUtil.colorize(title));
             obj.setDisplaySlot(DisplaySlot.SIDEBAR);
 
             int score = rawLines.size();
@@ -56,6 +66,7 @@ public class ScoreboardManager {
                     parsed = parsed.replace("%top_player_" + i + "_name%", name);
                     parsed = parsed.replace("%top_player_" + i + "_amount%", amount);
                 }
+                parsed = applyPapi(player, parsed);
                 String colored = HexUtil.colorize(parsed);
                 if (colored.length() > 40) colored = colored.substring(0, 40);
 
@@ -80,4 +91,4 @@ public class ScoreboardManager {
             player.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
         }
     }
-                  }
+                        }
