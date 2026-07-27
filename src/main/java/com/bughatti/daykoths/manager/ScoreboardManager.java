035bb7ebcff3d2ee80122052d755a80f93bd578a
bugh4tti <quieroamiabuu@gmail.com>
2026-07-27 09:52:13 -0300

    Implement ScoreboardManager for Koth game mode

---
+++ src/main/java/com/bughatti/daykoths/manager/ScoreboardManager.java
@@ -0,0 +1,83 @@
+package com.bughatti.daykoths.manager;
+
+import com.bughatti.daykoths.DayKoths;
+import com.bughatti.daykoths.model.Koth;
+import com.bughatti.daykoths.util.HexUtil;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+public class ScoreboardManager {
+
+    private final DayKoths plugin;
+
+    public ScoreboardManager(DayKoths plugin) {
+        this.plugin = plugin;
+    }
+
+    public void update(Koth koth, String timeLeft) {
+        if (!plugin.getConfig().getBoolean("scoreboard.enabled", false)) {
+            clearAll();
+            return;
+        }
+
+        String title = HexUtil.colorize(plugin.getConfig().getString("scoreboard.title", "&b&lDAYKOTHS"));
+        List<String> rawLines = plugin.getConfig().getStringList("scoreboard.lines");
+
+        List<Map.Entry<UUID, Integer>> top = koth.getScoreProgress().entrySet().stream()
+                .sorted((a, b) -> b.getValue() - a.getValue())
+                .collect(Collectors.toList());
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+            Objective obj = board.registerNewObjective("daykoths", "dummy", title);
+            obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+
+            int score = rawLines.size();
+            for (String line : rawLines) {
+                String parsed = line.replace("%koth%", koth.getName()).replace("%time_left%", timeLeft);
+                for (int i = 1; i <= 3; i++) {
+                    String name = "---";
+                    String amount = "0";
+                    if (top.size() >= i) {
+                        UUID id = top.get(i - 1).getKey();
+                        String realName = Bukkit.getOfflinePlayer(id).getName();
+                        name = realName == null ? "---" : realName;
+                        amount = String.valueOf(top.get(i - 1).getValue());
+                    }
+                    parsed = parsed.replace("%top_player_" + i + "_name%", name);
+                    parsed = parsed.replace("%top_player_" + i + "_amount%", amount);
+                }
+                String colored = HexUtil.colorize(parsed);
+                if (colored.length() > 40) colored = colored.substring(0, 40);
+
+                Team team = board.registerNewTeam("line" + score);
+                String entry = entryFor(score);
+                team.addEntry(entry);
+                team.setPrefix(colored);
+                obj.getScore(entry).setScore(score);
+                score--;
+            }
+
+            player.setScoreboard(board);
+        }
+    }
+
+    private String entryFor(int score) {
+        return org.bukkit.ChatColor.values()[Math.min(score, 15) % 16].toString();
+    }
+
+    public void clearAll() {
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.setScoreboard(Bukkit.getScoreboardManager().getMainScoreboard());
+        }
+    }
+                  }
