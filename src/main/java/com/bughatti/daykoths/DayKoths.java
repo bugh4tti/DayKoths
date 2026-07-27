66d950ec509d93b5e532a57937c8a8d6defc85f9
bugh4tti <quieroamiabuu@gmail.com>
2026-07-27 10:04:06 -0300

    Add BossBarManager and ScoreboardManager to DayKoths

--- src/main/java/com/bughatti/daykoths/DayKoths.java
+++ src/main/java/com/bughatti/daykoths/DayKoths.java
@@ -4,8 +4,11 @@ import com.bughatti.daykoths.commands.DayKothsCommand;
 import com.bughatti.daykoths.gui.GuiListener;
 import com.bughatti.daykoths.listeners.KothListener;
 import com.bughatti.daykoths.listeners.WandListener;
+import com.bughatti.daykoths.manager.BossBarManager;
 import com.bughatti.daykoths.manager.KothManager;
+import com.bughatti.daykoths.manager.ScoreboardManager;
 import com.bughatti.daykoths.manager.SelectionManager;
+import com.bughatti.daykoths.placeholder.DayKothsExpansion;
 import com.bughatti.daykoths.tasks.CaptureTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,6 +17,8 @@ public class DayKoths extends JavaPlugin {
     private static DayKoths instance;
     private KothManager kothManager;
     private SelectionManager selectionManager;
+    private BossBarManager bossBarManager;
+    private ScoreboardManager scoreboardManager;
 
     @Override
     public void onEnable() {
@@ -22,6 +27,8 @@ public class DayKoths extends JavaPlugin {
 
         kothManager = new KothManager(this);
         selectionManager = new SelectionManager();
+        bossBarManager = new BossBarManager(this);
+        scoreboardManager = new ScoreboardManager(this);
 
         DayKothsCommand command = new DayKothsCommand(this);
         getCommand("daykoths").setExecutor(command);
@@ -33,15 +40,24 @@ public class DayKoths extends JavaPlugin {
 
         new CaptureTask(this).runTaskTimer(this, 20L, 20L);
 
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") != null) {
+            new DayKothsExpansion(this).register();
+            getLogger().info("PlaceholderAPI detectado, variables de DayKoths registradas.");
+        }
+
         getLogger().info("DayKoths habilitado correctamente.");
     }
 
     @Override
     public void onDisable() {
         if (kothManager != null) kothManager.save();
+        if (bossBarManager != null) bossBarManager.removeAll();
+        if (scoreboardManager != null) scoreboardManager.clearAll();
     }
 
     public static DayKoths getInstance() { return instance; }
     public KothManager getKothManager() { return kothManager; }
     public SelectionManager getSelectionManager() { return selectionManager; }
+    public BossBarManager getBossBarManager() { return bossBarManager; }
+    public ScoreboardManager getScoreboardManager() { return scoreboardManager; }
 }
