ff9dc8499ed5206dee36f924e181e59502f5246a
bugh4tti <quieroamiabuu@gmail.com>
2026-07-26 23:09:35 -0300

    Implement KothListener for block placement and player death

---
+++ src/main/java/com/bughatti/daykoths/listeners/KothListener.java
@@ -0,0 +1,49 @@
+package com.bughatti.daykoths.listeners;
+
+import com.bughatti.daykoths.DayKoths;
+import com.bughatti.daykoths.model.Koth;
+import com.bughatti.daykoths.util.HexUtil;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class KothListener implements Listener {
+
+    private final DayKoths plugin;
+
+    public KothListener(DayKoths plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Material type = e.getBlock().getType();
+        boolean isWoolOrCloth = type.name().contains("WOOL") || type == Material.COBWEB;
+        if (!isWoolOrCloth) return;
+
+        for (Koth koth : plugin.getKothManager().getAll()) {
+            if (koth.isInside(e.getBlock().getLocation()) && !koth.isUtilitiesAllowed()) {
+                e.setCancelled(true);
+                e.getPlayer().sendMessage(HexUtil.colorize(plugin.getConfig().getString("messages.utilities-blocked")));
+                return;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        Player player = e.getEntity();
+        for (Koth koth : plugin.getKothManager().getAll()) {
+            if (koth.isInside(player.getLocation())) {
+                e.setKeepInventory(koth.isKeepInventory());
+                if (koth.isKeepInventory()) {
+                    e.getDrops().clear();
+                }
+                return;
+            }
+        }
+    }
+}
